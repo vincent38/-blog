2b0252827db24f6017edf8beda137582e0ca83a1
vincent38 <vincent3897@gmail.com>
2015-09-15 08:00:09 +0200

    Added BBCODE-like support on commentaires.php

--- vue/commentaires.php
+++ vue/commentaires.php
@@ -66,6 +66,15 @@
 											$ShowImg = AffichageImage($billets["image"]);
 											echo $ShowImg;
 										}
+										//BBCODE-like
+										$billets["contenu"] = preg_replace("#\[alert=(success|info|warning|danger)\](.+)\[/alert\]#isU","<div class=\"alert alert-$1\" role=\"alert\">$2</div>",$billets["contenu"]); //bold
+										$billets["contenu"] = preg_replace("#\[b\](.+)\[/b\]#isU","<strong>$1</strong>",$billets["contenu"]); //bold
+										$billets["contenu"] = preg_replace("#\[i\](.+)\[/i\]#isU","<em>$1</em>",$billets["contenu"]); //italic
+										$billets["contenu"] = preg_replace("#\[color=(blue|red|green|\#[a-z0-9]{6})\](.+)\[/color\]#isU","<span style=\"color: $1\">$2</strong>",$billets["contenu"]); //color
+										$billets["contenu"] = preg_replace("#url!(https?://[a-z0-9._/-]+[a-z0-9A-Z&\?._=-]*)#i", "<a href='$1'>$1</a>", $billets["contenu"]); // url (with url!)
+										$billets["contenu"] = preg_replace("#\[img=(.+)\](https?://[a-z0-9._/-]+[a-z0-9A-Z&\?._=-]*)\[/img\]#isU", "<img src='$2' alt='$1'/>", $billets["contenu"]); //images
+										$billets["contenu"] = preg_replace("#(\\\')#i","'", $billets["contenu"]); //'
+										//Fin du BBCODE-like
 									?>
 									<div class="caption">
 										<h2><?php echo $billets["titre"]; ?></h2>
