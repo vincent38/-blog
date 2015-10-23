d7d164ee59235f94c6f8e48ed6e607e4d387631e
vincent38 <vincent3897@gmail.com>
2015-10-23 22:02:34 +0200

    Resolved : duplication pseudos into admin

--- vue/admin.php
+++ vue/admin.php
@@ -15,9 +15,14 @@
 								</thead>
 								<tbody>
 									<?php
+									$latest = "set";
 									foreach ($users as $user)
 									{
-										echo "<tr><td>".$user["pseudo"]."</td><td>".$user["date_inscription"]."</td><td>Rang actuel : ".Ranking($user["pseudo"])." - <a href='edit_rank.php?id=".$user["id"]."'>Editer le rang</a></td></tr>";
+										if ($latest != $user["pseudo"])
+										{
+											echo "<tr><td>".$user["pseudo"]."</td><td>".$user["date_inscription"]."</td><td>Rang actuel : ".Ranking($user["pseudo"])." - <a href='edit_rank.php?id=".$user["id"]."'>Editer le rang</a></td></tr>";
+											$latest = $user["pseudo"];
+										}
 									}
 									?>
 								</tbody>
