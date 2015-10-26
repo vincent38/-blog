6ffa4b720e8b8b1f4808ecdcd8431cf041ac3dfd
vincent38 <vincent3897@gmail.com>
2015-10-26 14:28:25 +0100

    Public release v1.2 - improvements

--- modele/connexionsql.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-	Connexion à la DB via PDO
-	/!\ PENSER A CHANGER LES ID /!\
-*/
-
-$base = new PDO("mysql:host=localhost;dbname=test;charset=utf8","root","",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
