3c7ee3e870450f5869aa769fbe1b11bed793c942
vincent38 <vincent3897@gmail.com>
2015-10-27 14:44:09 +0100

    Added footer link

--- vue/includes/footer.php
+++ vue/includes/footer.php
@@ -8,7 +8,7 @@
 				?>
 				<br />
 				<footer class="breadcrumb">
-			        <p class="text-muted">Design du site : © Vincent AUBRIOT - Editions du bureau - 2015</p>
+			        <a href="http://editionsdubureau.ovh/blog/index.php"><p class="text-muted">Design du site : © Vincent AUBRIOT - Editions du bureau - 2015</p></a>
 			    </footer>
 
 				</div>
